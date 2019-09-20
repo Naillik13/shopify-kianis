878fb5cfeb4141feba1c519babfbea3c11d41279
killian galea <killiangalea@MacBook-Pro-de-killian.local>
2019-09-20 16:31:31 +0200

    Average basket

--- index.php
+++ index.php
@@ -54,7 +54,9 @@
 
     $orders = array_slice(getOrderList(),0,10);
     $products = [];
+    $total_amount = 0;
     foreach ($orders as &$order) :
+        $total_amount += $order['total_price'];
         foreach ($order['line_items'] as &$item) :
             if (array_key_exists($item['product_id'], $products)) {
                 $products[$item['product_id']]->number += 1;
@@ -66,6 +68,7 @@
             }
         endforeach;
     endforeach;
+    $average_basket = round($total_amount / count($orders), 2);
     usort($products, "cmp");
     $products = array_slice($products, 0, 5)
 
@@ -74,25 +77,31 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     </head>
     <body class="container mt-3">
         <div>
             <h2>Order list</h2>
-            <table style="width:100%">
+            <table id="orderTable" style="width:100%">
+            <thead>
                 <tr>
                     <th>Id</th>
                     <th>Amount</th>
                     <th>Date</th>
                     <th>Email</th>
                 </tr>
+            </thead>
+            <tbody>
                 <?php foreach ($orders as $order) : ?>
                     <tr>
                         <td><?php echo $order['id'] ?></td>
-                        <td><?php echo $order['total_price'] ?></td>
+                        <td><?php echo $order['total_price'] ?> €</td>
                         <td><?php echo $order['created_at'] ?></td>
                         <td><?php echo $order['email'] ?></td>
                     </tr>
                 <?php endforeach ?>
+            </tbody>
             </table>
         </div>
         <div class="mt-5">
@@ -104,5 +113,16 @@
                 <?php endforeach ?>
             </ul>
         </div>
+        <div class="mt-5">
+            <h2>Average Basket</h2>
+            <?php echo $average_basket; ?> €
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready( function () {
+                $('#orderTable').DataTable();
+            } );
+        </script>
     </body>
 </html>
